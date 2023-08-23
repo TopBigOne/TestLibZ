c67f4cadc00d0a325bc9c5e40b65b58bac2f3de0
ZhouGuangYa <chinagyzhou@gmail.com>
2023-08-23 16:25:19 +0800

    1: add jsoncpp.so and related include files;

--- app/src/main/cpp/JniUtils.h
+++ app/src/main/cpp/JniUtils.h
@@ -8,7 +8,7 @@
 
 #define IS_DEBUG
 #ifdef IS_DEBUG
-#define LOG_TAG ("fromNDK")
+#define LOG_TAG ("NativeHttp")
 #define LOGI(...) ((void)__android_log_print(ANDROID_LOG_INFO   , LOG_TAG, __VA_ARGS__))
 #define LOGE(...) ((void)__android_log_print(ANDROID_LOG_ERROR  , LOG_TAG, __VA_ARGS__))
 #else
